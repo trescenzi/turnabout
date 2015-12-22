38ea7d2d5f6971518b7b24f3cd4b30d205d01e25
Thom Crescenzi <tcrescenzi@factset.com>
2015-12-22 14:11:07 -0500

    chore(turnabout): remove logging

--- turnabout.js
+++ turnabout.js
@@ -1,5 +1,3 @@
-console.log("hey bro");
-console.log($('textarea#id_mainboard').text().split('\n'));
 var mainboard = $('textarea#id_mainboard');
 var mainTab = $('label[for="id_mainboard"]');
 var CARD_REGEX = /(\d?).*/;
@@ -22,6 +20,5 @@ function updateMainTab(cardCount) {
 updateMainTab(getCardCount(getMainboardText()));
 
 mainboard.keyup(function() {
-  console.log('leypress');
   updateMainTab(getCardCount(getMainboardText()));
 });
