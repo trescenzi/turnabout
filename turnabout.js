2e8344da900466edf0470ac2f090f2e629a108de
Thom Crescenzi <tcrescenzi@factset.com>
2015-12-22 14:17:59 -0500

    feat(turnabout): ignore blank lines

--- turnabout.js
+++ turnabout.js
@@ -3,11 +3,15 @@ var mainTab = $('label[for="id_mainboard"]');
 var CARD_REGEX = /(\d?).*/;
 
 function getMainboardText() {
-  return mainboard.val().split('\n');
+  return _.map(mainboard.val().split('\n'), _.trim);
 }
 
 function getCardCount(cardList) {
   return _.reduce(cardList, function(sum, card) {
+    if (card.length === 0 ) {
+      return sum;
+    }
+
     var cardCount = CARD_REGEX.exec(card)[1];
     return sum + (cardCount === '' ? 1 : parseInt(cardCount));
   }, 0);
