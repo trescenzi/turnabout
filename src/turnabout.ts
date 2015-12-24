ced85a7d5f42620b352d41f874c000d69a079c08
Thomas <trescenzi@gmail.com>
2015-12-23 23:34:46 -0500

    fix(cardCound): dont count sideboard on hearthstone

--- src/turnabout.ts
+++ src/turnabout.ts
@@ -3,12 +3,15 @@
 const path: string = window.location.pathname;
 if (path.indexOf('add') !== -1 || path.indexOf('update') !== -1) {
   const mainboard: Board = new Board('mainboard', 'Mainboard');
-  const sideboard: Board = new Board('sideboard', 'Sideboard');
   const maybeboard: Board = new Board('maybeboard', 'Maybeboard');
   const acquireboard: Board = new Board('acquireboard', 'Acquireboard');
 
   mainboard.updateTab();
-  sideboard.updateTab();
   maybeboard.updateTab();
   acquireboard.updateTab();
+
+  if (window.location.host.indexOf('hearthstone') === -1) {
+    const sideboard: Board = new Board('sideboard', 'Sideboard');
+    sideboard.updateTab();
+  }
 }
